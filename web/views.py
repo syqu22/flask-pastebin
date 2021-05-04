b70d7732ae4798d6f10f4ce7fc4aed344ff59ab4
syqu22 <alekkrecik@gmail.com>
2021-05-04 16:18:39 +0200

    Pastebins passwords are now saved as a cookie

--- web/views.py
+++ web/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask.helpers import make_response
 from flask_login import current_user
 from flask_login.utils import login_required
 from .models import Pastebin
@@ -16,29 +17,49 @@ def home():
       password = request.form.get("password")
 
       if check_pastebin(title, pastebin):
-         if private == "True":
-            new_pastebin = Pastebin(title=title, content=pastebin, password=password, user_id=None if current_user.is_anonymous else current_user.id)  
-         else:
-            new_pastebin = Pastebin(title=title, content=pastebin, user_id=None if current_user.is_anonymous else current_user.id)      
-         db.session.add(new_pastebin)
-         db.session.commit()
-         new_pastebin.link = PastebinUtil.base36_encode(new_pastebin.id)
-         db.session.commit()
-         flash("Pastebin added!", category="success")
-         return redirect(url_for("views.pastebins", link=new_pastebin.link))
+         new_pastebin = Pastebin(title=title, content=pastebin, user_id=None if current_user.is_anonymous else current_user.id)  
+
+      db.session.add(new_pastebin)
+      db.session.commit()
+      new_pastebin.link = PastebinUtil.base36_encode(new_pastebin.id)
+      response = make_response(redirect(url_for("views.pastebins", link=new_pastebin.link)))
+      if private == "True":
+         new_pastebin.password = password
+         response.set_cookie(new_pastebin.link, new_pastebin.password)   
+      db.session.commit()
+
+      flash("Pastebin added!", category="success")
+      return response
+         
    return render_template("home.html", user=current_user)
 
-@views.route("/<link>")
+@views.route("/<link>", methods=["GET", "POST"])
 def pastebins(link: str):
-   pastebin = Pastebin.query.filter_by(link=link).first()
-   if pastebin:
-      if pastebin.password == None:
-         return render_template("pastebin.html", user=current_user, pastebin=pastebin)
+   if request.method == "GET":
+      pastebin = Pastebin.query.filter_by(link=link).first()
+      password_cookie = request.cookies.get(link)
+      if pastebin:
+         if pastebin.password == None or password_cookie == pastebin.password:
+            return render_template("pastebin.html", user=current_user, pastebin=pastebin)
+         else:
+            return render_template("pastebin.html", user=current_user, pastebin=pastebin, password=pastebin.password)
       else:
-         return render_template("pastebin.html", user=current_user, pastebin=pastebin, password=pastebin.password)
-   else:
-      flash("Can't find pastebin.", category="error")
-      return redirect(url_for("views.home"))
+         flash("Can't find pastebin.", category="error")
+         return redirect(url_for("views.home"))
+   
+   if request.method == "POST":
+      pastebin = Pastebin.query.filter_by(link=link).first()
+      password = request.form.get("password")
+      response = make_response(render_template("pastebin.html", user=current_user, pastebin=pastebin))
+
+      if password == pastebin.password:
+         response.set_cookie(link, pastebin.password)
+         return response
+      else:
+         flash("Password is incorrect!", category="error")
+         return redirect(url_for("views.pastebins", link=link))
+
+
 
 @views.route("/user")
 @login_required
