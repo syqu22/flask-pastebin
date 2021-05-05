59222a8cbbe85fe5026edec407fbad7980942a49
syqu22 <alekkrecik@gmail.com>
2021-05-05 12:44:04 +0200

    Changed the way pastebin looks

---
+++ web/static/js/copy_all.js
@@ -0,0 +1,6 @@
+document.getElementById('copy-all').addEventListener('click', copy_all);
+
+function copy_all() {
+  document.querySelector("#pastebin").select();
+  document.execCommand("copy");
+}
